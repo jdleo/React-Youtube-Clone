376797fb43e6f2fdb8f64a9475727a951faba434
John Leonardo <jdleonardo5@icloud.com>
2019-11-16 13:22:04 -0800

    refactor youtube search function

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -2,10 +2,5 @@ import axios from 'axios';
 
 //export axios create function
 export default axios.create({
-    baseURL: 'https://googleapis.com/youtube/v3', 
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: process.env.REACT_APP_YOUTUBE_API_KEY
-    }
+    baseURL: 'https://googleapis.com/youtube/v3'
 });
