091f08c344c47e4e0199c6b2538c8c82c3e42c2d
John Leonardo <jdleonardo5@icloud.com>
2019-11-16 11:56:11 -0800

    clean up boilerplate

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App from React.Component {
+    render () {
+        return (
+            <h1>Youtube Clone</h1>
+        );
+    }
+}
+
+export default App;
