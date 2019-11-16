6db7189fdb34540b0f5a128dfc49717bf780bc3f
John Leonardo <jdleonardo5@icloud.com>
2019-11-16 14:32:23 -0800

    although messy, videos now load in component

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
+import { Paper, Typography } from '@material-ui/core';
+
 class VideoDetails extends React.Component {
+
     render () {
+        //first, get variables passed into props
+        const { video } = this.props;
+
+        //first, check if we have no video
+        if (!video) return <React.Fragment> Loading... </React.Fragment>
+
+        //get video source, and slap it in a youtube embed link
+        const videoSource = `https://www.youtube.com/embed/${video.id.videoId}`
+
+        console.log(videoSource);
+
         return (
-            <h1>Video Details component</h1>
+            <React.Fragment>
+                <Paper elevation={4} style={{height: "70%"}}>
+                    <iframe
+                        frameBorder="0"
+                        height="100%"
+                        width="100%"
+                        title="Video Player"
+                        src={videoSource}
+                    />
+                </Paper>
+                <Paper elevation={4} style={{padding: "15px"}}>
+
+                </Paper>
+            </React.Fragment>
         );
     }
 }
